bd1cda7cd7fdd0f1044fa392da79d995ce199ce0
Lhy <Lhy@qq.com>
2022-06-26 16:30:53 +0800

    first commit

---
+++ src/main/java/com/lhy/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.lhy.git;
+
+/**
+ * @author 李海洋
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,Git!");
+    }
+}
